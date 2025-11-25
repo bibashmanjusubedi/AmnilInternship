e1074c3310a33aa221038a9e92b0749eae666115
bibashmanjusubedi <acharabibash@outlook.com>
2025-11-25 23:53:27 +0545

    feat(auth):Added Dto for login and register

---
+++ ClinicManagement/DTOs/AuthRequests/LoginDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ClinicManagement.DTOs.AuthRequests
+{
+    /// <summary>
+    /// Data Transfer Object for user login.
+    /// </summary>
+    public class LoginDto
+    {
+        /// <summary>
+        /// See <see cref="ClinicManagement.DTOs.AuthRequests.RegisterDto.Email"/>.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// See <see cref="ClinicManagement.DTOs.AuthRequests.RegisterDto.Password"/>.
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
---
+++ ClinicManagement/DTOs/AuthRequests/RegisterDto.cs
@@ -0,0 +1,23 @@
+﻿namespace ClinicManagement.DTOs.AuthRequests
+{
+    /// <summary>
+    /// Data Transfer Object for registering a new user.
+    /// </summary>
+    public class RegisterDto
+    {
+        /// <summary>
+        /// The email address for the new user.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The password for the new user.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// The full name of the new user.
+        /// </summary>
+        public string FullName { get; set; }
+    }
+}
